1d58623e631883d5e2da6112a908f9c3bc1d4ecb
Affani Yusuf <affani14@gmail.com>
2024-11-04 23:15:34 +0700

    Commit tugasphp

--- Pertemuan 7/belajarphp2.php
+++ Pertemuan 7/belajarphp2.php
@@ -1,34 +1,34 @@
-<?echo "hello world!"; 
-echo $andi = "Andi"; 
+<? echo "hello world!\n";
+echo $andi = "Andi";
 echo $andi;
 $i;
 $nama;
 $umur;
 $_lokasi_memori;
 $Angka_MAKSIMUM;
-$a='Saya sedang belajar php';
-$b='5';
+$a = 'Saya sedang belajar php';
+$b = '5';
 print $a;
 echo $b;
 $c = 5;
 $d = "Sedang belajar php $c";
-$umur=21;
-$harga=15000;
-$rugi=-50000;
+$umur = 21;
+$harga = 15000;
+$rugi = -50000;
 echo "<br/>";
 echo $harga;
 echo "<br/>";
 echo $rugi;
-$f=14;
-$g=16;
-$h=$f+$g;
+$f = 14;
+$g = 16;
+$h = $f + $g;
 echo $h;
 $i = $f * $g;
 echo $i;
-$angka_float1= 0.78;
-$angka_float2= 14.99;
-$angka_scientific1=0.314E1;
-$angka_scientific2=0.3365E-3;
+$angka_float1 = 0.78;
+$angka_float2 = 14.99;
+$angka_scientific1 = 0.314E1;
+$angka_scientific2 = 0.3365E-3;
 echo $angka_float1;
 echo "<br/>";
 echo $angka_float2;
@@ -38,25 +38,45 @@ echo "<br/>";
 echo $angka_scientific2;
 echo "<br/>";
 
-$j=10.66;
-$k=12.44;
-$l= $j + $k;
+$j = 10.66;
+$k = 12.44;
+$l = $j + $k;
 echo $k;
-$l=$j * $k;
+$l = $j * $k;
 echo $l;
 
-$string1='Ini adalah string sederhana';
-$string2="Ini adalah string yang bisa memiliki beberapa garis";
+$string1 = 'Ini adalah string sederhana';
+$string2 = "Ini adalah string yang bisa memiliki beberapa garis";
 
-$stirng3='Dia berkata: "I will back"';
+$string3 = 'Dia berkata: "I will back"';
 
-echo $string1; echo "<br/>";
-echo $string2; echo "<br/>";
-echo $string3; echo "<br/>";
+echo $string1;
+echo "<br/>";
+echo $string2;
+echo "<br/>";
+echo $string3;
+echo "<br/>";
 
-$benar=true;
-$salah=false;
+$benar = true;
+$salah = false;
 
 echo "benar = $benar, salah = $salah";
 
-?>
+$hasil = -3;
+$hasil2 = 3+5;
+$hasil3 = 8 - 4.5;
+$hasil4 = 2 * 5;
+$hasil5 =  3 + 8 /5-3;
+$hasil6 = 10 % 4;
+
+echo "$hasil:"; var_dump($hasil);
+echo "<br\>";
+echo "$hasil2:"; var_dump($hasil2);
+echo "<br/>";
+echo "$hasil3:"; var_dump($hasil3);
+echo "<br/>";
+echo "$hasil4:"; var_dump($hasil4);
+echo "<br/>";
+echo "$hasil5:"; var_dump($hasil5);
+echo "<br/>";
+echo "$hasil6:"; var_dump($hasil6);
--- Pertemuan 7/tugasphp.php
+++ Pertemuan 7/tugasphp.php
@@ -1,13 +1,16 @@
-<?php $a=50000000;
-$b =16500000;
+<?php $a = 50000000;
+$b = 16500000;
 $c = 9500000;
-$d = 5/100;
-$e = 4.5/100;
+$d = 5 / 100;
+$e = 4.5 / 100;
 
-$sisa = $a-($b+($b*$d)) - ($c-($c*$e));
-$tbunga = (($b*$d)) + ($c*$e);
-$thutang = ($b+($b*$d)) + ($c-($c*$e));
+$sisa = $a - ($b + ($b * $d)) - ($c + ($c * $e));
+$tbunga = (($b * $d)) + ($c * $e);
+$thutang = ($b + ($b * $d)) + ($c + ($c * $e));
 
-echo "sisa uang:"; echo ($sisa)."<br/>";
-echo "$tbunga:"; var_dump($tbunga). "<br/>";
-echo "$thutang:"; var_dump($thutang). "<br/>";
+echo "Sisa uang: ";
+echo ($sisa) . "<br/>";
+echo "Total bunga: ";
+echo ($tbunga) . "<br/>";
+echo "Total hutang: ";
+echo ($thutang) . "<br/>";
