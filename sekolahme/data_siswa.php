1501c91331ebd57f9178cc676d18ed576cd41b17
Affani <affani14@gmail.com>
2024-12-02 12:48:13 +0700

    Update data_siswa.php

--- sekolahme/data_siswa.php
+++ sekolahme/data_siswa.php
@@ -4,8 +4,8 @@ include 'koneksi.php';
 <!DOCTYPE html>
 <html lang="en">
     <head>
-        <meta charset="UTF-8>
-        <meta http-equiv="X-UA-Compatible" content="IE=edge>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1.0">
         <title>Document</title>
     </head>
@@ -21,24 +21,21 @@ include 'koneksi.php';
         <td>Aksi</td>
     </tr>
         <?php 
-            $no = '1';
-            $query = mysqli_query($konek, "Select * from data_siswa");
+            $no = 1;
+            $query = mysqli_query($konek, "SELECT * FROM data_siswa");
             while($data = mysqli_fetch_array($query)) {
-
+        ?>
+            <tr>
+                <td><?php echo $no++; ?></td>
+                <td><?php echo $data['nama_siswa']; ?></td>
+                <td><?php echo $data['alamat_siswa']; ?></td>
+                <td><?php echo $data['agama_siswa']; ?></td>
+                <td><?php echo $data['asal_sekolah']; ?></td>
+                <td><a href="">Edit</a> <a href="">Hapus</a></td>
+            </tr>
+        <?php
             }
         ?>
-    <tr>
-        <td><?php echo $no ++;?></td>
-		<td><?php echo $data ['nama_siswa'];?></td>
-		<td><?php echo $data ['alamat_siswa'];?>></td>
-		<td><?php echo $data ['agama_siswa'];?></td>
-		<td><?php echo $data ['asal_sekolah'];?></td>
-		<td><a href="">Edit</a> <a href="">Hapus</a></td>
-	</tr>
-	
-	<?php
-	}
-	?>
     </table>
 </body>
 </html>
