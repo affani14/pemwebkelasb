4b9f77c7d2e18bd8eeea8838148628214ee7e637
Affani Yusuf <affani14@gmail.com>
2025-01-20 14:15:10 +0700

    commit pie

---
+++ civitaskuisoner/register.php
@@ -0,0 +1,39 @@
+<?php
+include 'config.php';
+
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    $username = $_POST['username'];
+    $password = password_hash($_POST['password'], PASSWORD_DEFAULT);
+
+    $sql = "INSERT INTO users (username, password) VALUES ('$username', '$password')";
+    if ($conn->query($sql) === TRUE) {
+        header("Location: index.php");
+    } else {
+        echo "Error: " . $conn->error;
+    }
+}
+?>
+
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title>Register</title>
+    <link rel="stylesheet" href="styles/style.css">
+
+</head>
+
+<body>
+    <h1>Register Civitas Kuisoner</h1>
+    <form method="POST">
+        <label>Username:</label>
+        <input type="text" name="username" required>
+        <br>
+        <label>Password:</label>
+        <input type="password" name="password" required>
+        <br>
+        <button type="submit">Register</button>
+    </form>
+</body>
+
+</html>
